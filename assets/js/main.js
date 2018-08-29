e0726213f2cd65b6f9c8d925a60179adb1eadcd8
Timibadass <oblacishere@gmail.com>
2018-08-29 22:55:55 +0100

    made the active nav dynamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,17 @@ const navLink = Array.prototype.slice.call(navLinkNodeList);
 const topDiv = document.querySelector('#top');
 const middleDiv = document.querySelector('#middle');
 const bottomDiv = document.querySelector('#bottom');
+const homeDiv = document.querySelector('#home');
+// const homeDivPosition = homeDiv.offsetTop;
+const aboutDiv = document.querySelector('#about');
+// const aboutDivPosition = aboutDiv.offsetTop;
+const contactUsDiv = document.querySelector('#contact_us');
+// const contactUsDivPosition = contactUsDiv.offsetTop
+const blogDiv = document.querySelector('#blog');
+// const blogDivPosition = blogDiv.offsetTop;
+const coreTeamDiv = document.querySelector('#core_team');
+// const coreTeamDivPosition = coreTeamDiv.offsetTop;
+const pageSection = [homeDiv, aboutDiv, contactUsDiv, blogDiv, coreTeamDiv];
 
 const animateNavButton = function () {
     if(screen.width < 850) {
@@ -37,3 +48,24 @@ navLink.forEach(function(link) {
     link.addEventListener('click', animateNavButton);
     link.addEventListener('click', MakeActive);
 });
+
+
+window.addEventListener('scroll', function() {
+    const windowPosition = window.scrollY;
+    pageSection.forEach(function(section) {
+        // the number 90 is to add the active class at the right time
+        let sectionBorder = section.offsetTop + section.scrollHeight - 90;
+        let hashPlusSectionId = '#' + section.id ;
+        if(windowPosition >= section.offsetTop && windowPosition <= sectionBorder) {
+            navLink.forEach(function(link) {
+                let linkAttribute = link.querySelector('a').getAttribute('href');
+                // console.log(linkAttribute, hashPlusSectionId);
+                if( hashPlusSectionId === linkAttribute) {
+                    link.classList.add('active_link');
+                } else {
+                    link.classList.remove('active_link');
+                }
+            })
+        }
+    });
+})
